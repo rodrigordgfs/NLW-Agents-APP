ef0a6a2a0200fa569dcd7e3db4fdf1b6e7d3ed8c
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-07-09 17:35:01 -0300

    feature: Configurado o Biome

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './app'
 
+// biome-ignore lint/style/noNonNullAssertion: <Mandatory assertion for root element>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
