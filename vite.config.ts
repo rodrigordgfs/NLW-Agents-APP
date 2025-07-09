dc070cdc305c397368dc96137b9f74865f09ece8
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-07-09 17:40:39 -0300

    feature: Instalado o Tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// biome-ignore assist/source/organizeImports: <Organize imports in Vite config file>
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
