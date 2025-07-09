e459467488a632f289f7ffafb52db1d21369fe69
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2025-07-09 17:48:10 -0300

    feature: Instalado o ShadcnUi

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export const App = () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <Button variant="secondary">Click me</Button>
+    </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 // biome-ignore assist/source/organizeImports: <Organize imports in Vite config file>
 import { defineConfig } from 'vite'
+import path from "node:path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
